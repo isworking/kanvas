f6f1bee5554a41576ef0f833e53f68281ded877c
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2026-05-11 22:09:46 +0530

    build: basic versioning setup
    Introduce basic versioning setup in both CMake and headers.
    
    Signed-off-by: Rajdeep Malakar <rajdeepm.dev@gmail.com>

--- include/kvs/kvs.h
+++ include/kvs/kvs.h
@@ -5,6 +5,17 @@
 #ifndef KVS_H
 #define KVS_H
 
+#define KVS_VERSION_MAJOR 0
+#define KVS_VERSION_MINOR 0
+#define KVS_VERSION_PATCH 1
+
+#define KVS_VERSION              \
+    ((KVS_VERSION_MAJOR << 16) | \
+     (KVS_VERSION_MINOR << 8) |  \
+     KVS_VERSION_PATCH)
+
+#define KVS_VERSION_STRING "0.0.1"
+
 #include <kvs/canvas.h>
 #include <kvs/color.h>
 #include <kvs/drawable.h>
